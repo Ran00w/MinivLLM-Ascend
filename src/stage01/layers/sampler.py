66da6983f972504f61c2173ebe445ec4aca4e05f
Ran0 <202330350741@mail.scut.edu.cn>
2026-05-06 11:56:31 +0800

    docs: finish stage01

--- src/stage01/layers/sampler.py
+++ src/stage01/layers/sampler.py
@@ -1,13 +1,15 @@
 import torch
+import torch.nn as nn
 
-class Sampler:
+class Sampler(nn.Module):
 
     def __init__(self):
         super().__init__()
 
+    @torch.compile
     def sample(self, logits):
         if logits.ndim != 2:
-            raise ValueError("logit must have the shape of [batch,]")
+            raise ValueError("logit must have the shape of [batch,vocab_size]")
         probs = torch.softmax(logits, dim=-1)
         sample_tokens = torch.argmax(probs, dim=-1)
         return sample_tokens
